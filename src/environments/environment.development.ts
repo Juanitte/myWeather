f15857dffef55d348ab78ba44e76adba6bb249fe
Juanite <juan13_barsa@hotmail.com>
2023-11-23 08:56:24 +0100

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    apis:{
+        weather: {
+            url:'https://api.weatherapi.com/v1/forecast.json?days=3&aqi=no&alerts=no',
+            key:'e869407cb065496a97d113812231411',
+            testPosition:'37.6593416,-4.7220263'
+        }
+    }
+};
